1fea7dc44a8fe8d29a831e55ffb8f9579d48fbdb
vinicius <viniciusneiva125@gmail.com>
2023-11-21 09:03:46 -0300

    adição classe aluno

---
+++ src/main/java/br/edu/ifpi/entidades/Aluno.java
@@ -0,0 +1,38 @@
+package br.edu.ifpi.entidades;
+
+public class Aluno {
+    private String nome;
+    private String id;
+    private String email;
+
+    public Aluno(String nome, String id, String email) {
+        this.nome = nome;
+        this.id = id;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
