32b5b069a8dfc36ace2c2bcfe37107743e7c42c4
vinicius <viniciusneiva125@gmail.com>
2023-11-21 08:51:24 -0300

    Main por vini

---
+++ src/main/java/br/edu/ifpi/sistema/Curso.java
@@ -0,0 +1,40 @@
+package br.edu.ifpi.sistema;
+
+public class Curso {
+    private String nome;
+    private boolean status;
+    private int cargaHoraria;
+
+    
+    public Curso(String nome, boolean status, int cargaHoraria) {
+        this.nome = nome;
+        this.status = status;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+}
