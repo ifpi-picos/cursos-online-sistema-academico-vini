4d6bed51888b86cceeb8ac9a368c8d11a262cb04
vinicius <viniciusneiva125@gmail.com>
2023-11-21 11:47:16 -0300

    atualização

--- src/main/java/br/edu/ifpi/enums/StatusCurso.java
+++ src/main/java/br/edu/ifpi/enums/StatusCurso.java
@@ -1,5 +1,16 @@
 package br.edu.ifpi.enums;
 
 public enum StatusCurso {
-  ABERTO, FECHADO
+    ABERTO("Aberto"),
+    FECHADO("Fechado");
+
+    private final String descricao;
+
+    StatusCurso(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
