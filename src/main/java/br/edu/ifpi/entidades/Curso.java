1f34011c0ecc0842fba25d7bab341bcc73c8b336
vinicius <viniciusneiva125@gmail.com>
2023-11-21 09:28:04 -0300

    atualização classe curso

--- src/main/java/br/edu/ifpi/entidades/Curso.java
+++ src/main/java/br/edu/ifpi/entidades/Curso.java
@@ -1,41 +1,51 @@
-//classe curso
+
 package br.edu.ifpi.entidades;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Curso {
     private String nome;
     private boolean status;
     private int cargaHoraria;
+    private List<Aluno> alunosMatriculados;
 
-    
     public Curso(String nome, boolean status, int cargaHoraria) {
         this.nome = nome;
         this.status = status;
         this.cargaHoraria = cargaHoraria;
+        this.alunosMatriculados = new ArrayList<>();
     }
 
-    
     public String getNome() {
         return nome;
     }
 
-    public boolean getStatus() {
-        return status;
-    }
-
-    public int getCargaHoraria() {
-        return cargaHoraria;
-    }
-
-    
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public boolean isStatus() {
+        return status;
     }
 
     public void setStatus(boolean status) {
         this.status = status;
     }
 
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
     public void setCargaHoraria(int cargaHoraria) {
         this.cargaHoraria = cargaHoraria;
+    }
+
+    public List<Aluno> getAlunosMatriculados() {
+        return alunosMatriculados;
+    }
+
+    public void matricularAluno(Aluno aluno) {
+        alunosMatriculados.add(aluno);
     }
 }
