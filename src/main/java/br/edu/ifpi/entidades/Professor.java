83fcd2c79dccc73c650366d7f8b2b048190276a6
vinicius <viniciusneiva125@gmail.com>
2023-11-22 14:42:08 -0300

    atualizção de classe

--- src/main/java/br/edu/ifpi/entidades/Professor.java
+++ src/main/java/br/edu/ifpi/entidades/Professor.java
@@ -1,51 +1,24 @@
 package br.edu.ifpi.entidades;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class Professor {
-    private String nome;
-    private String id;
-    private String email;
-    private List<Curso> cursosMinistrados;
+public class Professor extends Usuario {
+    private List<String> disciplinasMinistradas;
 
-    public Professor(String nome, String id, String email) {
-        this.nome = nome;
-        this.id = id;
-        this.email = email;
-        this.cursosMinistrados = new ArrayList<>();
+    public Professor(String nome, String id, String email, String senha) {
+        super(nome, id, email, "professor", senha);
     }
 
-
-    public String getNome() {
-        return nome;
+    public void associarDisciplina(String disciplina) {
+        disciplinasMinistradas.add(disciplina);
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public List<Curso> getCursosMinistrados() {
-        return cursosMinistrados;
-    }
-
-    public void associarCurso(Curso curso) {
-        cursosMinistrados.add(curso);
+    public void darNota(Aluno aluno, String disciplina, int nota) {
+        if (disciplinasMinistradas.contains(disciplina)) {
+            aluno.receberNota(disciplina, nota);
+            System.out.println("Nota atribuída com sucesso!");
+        } else {
+            System.out.println("Professor não ministra a disciplina " + disciplina);
+        }
     }
 }
