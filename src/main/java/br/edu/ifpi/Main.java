ec1bd9613791cebed326e084ebebd58b78332c2c
vinicius <viniciusneiva125@gmail.com>
2023-11-25 01:21:49 -0300

    estrutura base

--- src/main/java/br/edu/ifpi/Main.java
+++ src/main/java/br/edu/ifpi/Main.java
@@ -1,50 +1,46 @@
 package br.edu.ifpi;
-
 import br.edu.ifpi.entidades.Aluno;
-import br.edu.ifpi.entidades.Autenticacao;
-import br.edu.ifpi.entidades.Autorizacao;
 import br.edu.ifpi.entidades.Curso;
-import br.edu.ifpi.entidades.Disciplina;
-import br.edu.ifpi.entidades.Nota;
 import br.edu.ifpi.entidades.Professor;
-import br.edu.ifpi.enums.StatusCurso;
 import br.edu.ifpi.entidades.Usuario;
-
+import br.edu.ifpi.dao.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-     private static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         List<Usuario> usuarios = new ArrayList<>();
         Curso cursoJava = new Curso("Programação Java", "Aberto", 40);
 
-        // exemplo
-        Aluno aluno1 = new Aluno("Joao", "aluno1", "joao@email.com", null);
+        // Exemplo de usuários
+        Aluno aluno1 = new Aluno("vinicius", "aluno1", "vini@email.com", null);
         aluno1.setSenha("senhaAluno1");
 
         Professor professor1 = new Professor("ProfSilva", "professor1", "silva@email.com", null);
         professor1.setSenha("senhaProfessor1");
 
-
         usuarios.add(aluno1);
         usuarios.add(professor1);
 
-    
-        realizarLogin(usuarios, cursoJava);
+        realizarLogin(cursoJava, usuarios);
     }
 
-    private static void realizarLogin(List<Usuario> usuarios, Curso curso) {
+    private static void realizarLogin(Curso curso, List<Usuario> usuarios) {
         System.out.print("Digite o nome de usuário: ");
         String nomeUsuario = scanner.nextLine();
         System.out.print("Digite a senha: ");
         String senha = scanner.nextLine();
 
-        // Verifica se o usuário existe e valida a senha
-        Usuario usuario = buscarUsuario(usuarios, nomeUsuario, senha);
+        
+        Usuario usuario = buscarUsuario(nomeUsuario, senha);
         if (usuario != null) {
             if (usuario instanceof Aluno) {
                 menuAluno((Aluno) usuario, curso);
@@ -55,15 +51,26 @@ public class Main {
             }
         } else {
             System.out.println("Credenciais inválidas. Tente novamente.");
-            realizarLogin(usuarios, curso);
+            realizarLogin(curso, usuarios);
         }
     }
 
-    private static Usuario buscarUsuario(List<Usuario> usuarios, String nomeUsuario, String senha) {
-        for (Usuario usuario : usuarios) {
-            if (usuario.getNome().equals(nomeUsuario) && usuario.validarSenha(senha)) {
-                return usuario;
+    private static Usuario buscarUsuario(String nomeUsuario, String senha) {
+        String sql = "SELECT * FROM usuarios WHERE nome = ? AND senha = ?";
+        try (Connection connection = Conexao.getConexao();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, nomeUsuario);
+            statement.setString(2, senha);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    
+                    return new Usuario(resultSet.getString("nome"), resultSet.getString("email"), sql, sql);
+                }
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return null;
     }
@@ -78,20 +85,16 @@ public class Main {
             System.out.println("3. Sair");
             System.out.print("Escolha uma opção: ");
             opcao = scanner.nextInt();
-            scanner.nextLine(); 
+            scanner.nextLine();
 
             switch (opcao) {
                 case 1:
-                    aluno.matricularEmDisciplina("Matemática");
-                    aluno.matricularEmDisciplina("História");
-                    aluno.matricularEmDisciplina("Programação");
-                    System.out.println("Matemática");
-                    System.out.println("História");
-                    System.out.println("Programação");
+                    
+                    System.out.println("Implemente a lógica para exibir as disciplinas matriculadas do banco de dados");
                     break;
                 case 2:
-                    aluno.verDisciplinasMatriculadas();
-                    aluno.verNotas();
+                    
+                    System.out.println("Implemente a lógica para exibir as notas do banco de dados");
                     break;
                 case 3:
                     System.out.println("Saindo do sistema. Até logo!");
@@ -100,7 +103,7 @@ public class Main {
                     System.out.println("Opção inválida. Tente novamente.");
             }
 
-            System.out.println(); // Adiciona uma linha em branco para melhorar a leitura
+            System.out.println(); 
 
         } while (opcao != 3);
     }
@@ -115,30 +118,23 @@ public class Main {
             System.out.println("3. Sair");
             System.out.print("Escolha uma opção: ");
             opcao = scanner.nextInt();
-            scanner.nextLine();  // Limpar o buffer do scanner
+            scanner.nextLine();  
 
             switch (opcao) {
                 case 1:
                     System.out.print("Digite o nome da disciplina: ");
-                    String disciplina = scanner.nextLine();
-                    professor.associarDisciplina(disciplina);
-                    System.out.println("Disciplina associada com sucesso!");
+                    scanner.nextLine();
+                    
+                    System.out.println("Implemente a lógica para associar disciplina no banco de dados");
                     break;
                 case 2:
                     System.out.print("Digite o ID do aluno: ");
-                    String idAluno = scanner.nextLine();
                     System.out.print("Digite a disciplina: ");
-                    disciplina = scanner.nextLine();
+                    scanner.nextLine();
                     System.out.print("Digite a nota: ");
-                    int nota = scanner.nextInt();
-                    scanner.nextLine();  // Limpar o buffer do scanner
-
-                    Aluno aluno = procurarAluno(curso, idAluno);
-                    if (aluno != null) {
-                        professor.darNota(aluno, disciplina, nota);
-                    } else {
-                        System.out.println("Aluno não encontrado.");
-                    }
+                    scanner.nextLine();  
+                    
+                    System.out.println("Implemente a lógica para dar nota no banco de dados");
                     break;
                 case 3:
                     System.out.println("Saindo do sistema. Até logo!");
@@ -147,17 +143,10 @@ public class Main {
                     System.out.println("Opção inválida. Tente novamente.");
             }
 
-            System.out.println(); // Adiciona uma linha em branco para melhorar a leitura
+            System.out.println(); 
 
         } while (opcao != 3);
     }
 
-    private static Aluno procurarAluno(Curso curso, String idAluno) {
-        for (Aluno aluno : curso.getAlunoMatriculado()) {
-            if (aluno.getId().equals(idAluno)) {
-                return aluno;
-            }
-        }
-        return null;
-    }
+    
 }
