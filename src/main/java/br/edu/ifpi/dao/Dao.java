bdc39757976e92c16bd53b36464cf659ed75d6ac
vinicius <viniciusneiva125@gmail.com>
2023-11-28 15:33:43 -0300

    atualização

--- src/main/java/br/edu/ifpi/dao/Dao.java
+++ src/main/java/br/edu/ifpi/dao/Dao.java
@@ -3,14 +3,9 @@ package br.edu.ifpi.dao;
 import java.util.List;
 
 public interface Dao<T> {
-
-
-  public int cadastrar(T entidade);
-
-  public List<T> consultarTodos();
-
-  public int alterar(T entidade);
-
-  public int remover(T entidade);
-
+    List<T> consultarTodos();
+    int cadastrar(T entidade);
+    int alterar(T entidade);
+    int remover(T entidade);
+    T buscarPorId(String id);
 }
